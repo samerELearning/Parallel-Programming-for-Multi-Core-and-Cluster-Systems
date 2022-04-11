d31b81d4eb44664bb3aba756bcaedf24d6e37ef1
samerelearning <samerelearning@outlook.com>
2022-04-12 01:06:25 +0300

    declaring and initializing variables

--- Lab 1/solution.c
+++ Lab 1/solution.c
@@ -3,5 +3,24 @@
 
 int main()
 {
-	
+	int		i;//number of iterations
+	double  duration;
+	double  pi;
+	clock_t start;
+	clock_t end;
+	int 	counter;
+
+	i 		= 1000;
+	start	= clock();
+	counter = 0;
+
+	srand(time(NULL));
+	//loop
+
+	end		= clock();
+	duration= ((double)(end - start) / CLOCKS_PER_SEC) * 1000;
+	pi 		= 4.0 * ((double)counter / i);
+	printf("Serial of pi is %f and it takes %fms\n", pi, duration);
+
+	return 0;
 }
