aa137a4eecc21a4ade1f7c6ef8721ec46abe1c7d
samerelearning <samerelearning@outlook.com>
2022-04-12 13:30:01 +0300

    initializing variables

--- lab 2/serial-code.c
+++ lab 2/serial-code.c
@@ -10,9 +10,15 @@ int main()
 	clock_t		start;
 	clock_t		end;
 
+	array_size	= 1000;
+	counter		= 0;
+	rand_arr	= calloc(array_size, sizeof(int));
+	srand(time(NULL));
+	start		= clock();
 
 	//for loop
 
-
+	end		= clock();
+	duration= ((double)(end - start) / CLOCKS_PER_SEC) * 1000;
 	return 0;
 }
